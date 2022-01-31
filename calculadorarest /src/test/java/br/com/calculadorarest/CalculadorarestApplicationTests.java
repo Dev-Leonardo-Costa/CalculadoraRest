42823d220756746717e08a4dcffcea61ad03c88b
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2022-01-31 14:57:00 -0300

    primeiro commit do projeto

---
+++ calculadorarest /src/test/java/br/com/calculadorarest/CalculadorarestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadorarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadorarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
