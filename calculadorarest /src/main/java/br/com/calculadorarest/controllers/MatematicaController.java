4f7c2422e19b9f6e0cb7d5a54fad2353b1f0b7f5
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2022-01-31 16:58:13 -0300

    foi desenvolvido a função somar

---
+++ calculadorarest /src/main/java/br/com/calculadorarest/controllers/MatematicaController.java
@@ -0,0 +1,33 @@
+package br.com.calculadorarest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MatematicaController {
+
+    @RequestMapping(value = "/somar/{primeiroNumero}/{segundoNumero}", method = RequestMethod.GET)
+    public Double soma(@PathVariable("primeiroNumero") String primeiroNumero, @PathVariable("segundoNumero") String segundoNumero) throws Exception {
+        if (!eNumerico(primeiroNumero) || !eNumerico(segundoNumero)) {
+            throw new Exception();
+        }
+        Double soma = coverterDouble(primeiroNumero) + coverterDouble(segundoNumero);
+        return soma;
+    }
+
+    private Double coverterDouble(String strNumero) {
+        if (strNumero == null) return 0D;
+        String numero = strNumero.replaceAll(",",".");
+        if (eNumerico(numero)) return  Double.parseDouble(numero);
+        return 0D;
+    }
+
+    private boolean eNumerico(String strNumero) {
+        if (strNumero == null) return false;
+        String numero = strNumero.replaceAll(",",".");
+        return numero.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
---
+++ calculadorarest /src/main/java/model/MatematicaSimples.java
@@ -0,0 +1,20 @@
+package model;
+
+public class MatematicaSimples {
+    
+    public Double somar(Double primeiroNumero, Double segundoNumero){
+        return primeiroNumero + segundoNumero;
+    }
+
+    public Double subtrair(Double primeiroNumero, Double segundoNumero){
+        return primeiroNumero - segundoNumero;
+    }
+
+    public Double dividir(Double primeiroNumero, Double segundoNumero){
+        return primeiroNumero / segundoNumero;
+    }
+
+    public Double multiplicar(Double primeiroNumero, Double segundoNumero){
+        return primeiroNumero * segundoNumero;
+    }
+}
